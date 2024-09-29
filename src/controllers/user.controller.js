2d963ada1afacea509f294e479fe9dd8fab7e131
Abir Khan <ak99.abirkhan@gmail.com>
2024-09-30 02:55:48 +0600

    auth.middleware and logout

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -152,13 +152,38 @@ const loginUser = asyncHandler( async (req, res) => {
 
 const logoutUser = asyncHandler( async(req, res) => {
 
-    
-})
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+
+    const options = {
+
+        httpOnly: true,
+        secure: true
+    }
+
+    return res.status(200)
+        .cookie("accessToken", options)
+        .cookie("refreshToken", options)
+        .json(
+            new ApiResponse(200, {}, "User logged out successfully")
+        )
+});
 
 
 
 
 export { 
     registerUser,
-    loginUser
+    loginUser,
+    logoutUser
 };
